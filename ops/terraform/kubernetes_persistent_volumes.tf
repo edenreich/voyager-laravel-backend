6028560508bd73dff0080450b5b9ef4e37b6975e
Eden Reich <eden.reich@gmail.com>
2019-08-12 01:20:33 +0200

    reuse same size of volume in volume claim

--- ops/terraform/kubernetes_persistent_volumes.tf
+++ ops/terraform/kubernetes_persistent_volumes.tf
@@ -3,14 +3,19 @@
 resource "kubernetes_persistent_volume_claim" "db_data" {
   metadata {
     name = "db-data"
+    namespace = "default"
   }
+
   spec {
     access_modes = ["ReadWriteMany"]
+
     resources {
       requests = {
-        storage = "20Gi"
+        storage = "${digitalocean_volume.db_data.size}Gi"
       }
     }
+
     volume_name = "${digitalocean_volume.db_data.name}"
+    storage_class_name = "do-block-storage"
   }
 }
